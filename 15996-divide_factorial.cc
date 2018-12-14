9be50cb816edadb832af5ee6b4c7ade75943565a
moreal <dev.moreal@gmail.com>
2018-12-14 20:20:52 +0900

    Solve 15996 Divide_Factorial Problem with cpp

---
+++ 15996-divide_factorial.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int N, K, cnt { 0 };
+    std::cin >> N >> K;
+
+    // uK meaning useful K
+    for (long long uK { K }; uK < N; uK *= K) {
+        cnt += N / uK;
+    }
+
+    std::cout << cnt;
+}
